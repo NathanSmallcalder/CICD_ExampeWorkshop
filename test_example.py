86bae394bbf029c81cffd908e3a7a89d13162d1d
Nathan Smallcalder <nsmallcalder5@gmail.com>
2021-12-02 10:52:45 +0000

    Test Complete

--- test_example.py
+++ test_example.py
@@ -1,10 +1,12 @@
 from example import *
 
+
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -2) == -1
     assert add(1000000000000000000, 2) == 1000000000000000002
 
+
 def test_multiply():
-    assert multiply(1*1) == 1
-    assert multiply(2*2) == 4
+    assert multiply(1, 1) == 1
+    assert multiply(2, 2) == 4
